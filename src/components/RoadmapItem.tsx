a13ae638344b72b71ac60bae6d6e38ea4d21491b
fastendev <juniel.liu1015@gmail.com>
2022-04-14 00:36:19 -0400

    sort the items

--- src/components/RoadmapItem.tsx
+++ src/components/RoadmapItem.tsx
@@ -7,7 +7,7 @@ interface RoadmapItemProps {
 
 export default function RoadmapItem({ title, content }: RoadmapItemProps) {
     return (
-        <div className="h-56 sm:h-72 rounded-2xl p-8 sm:p-5 md:p-8 m-2 flex flex-col" style={{ boxShadow: '0px 10px 20px 0px #00000026' }}>
+        <div className="h-60 sm:h-72 rounded-2xl p-4 sm:p-5 md:p-8 m-2 flex flex-col space-y-2" style={{ boxShadow: '0px 10px 20px 0px #00000026' }}>
             <div className='flex flex-row justify-between sm:flex-col items-center sm:items-start '>
                 <div className="text-xl">{title}</div>
                 <img className='w-6 sm:w-20' src={circle}></img>
--- src/sections/Roadmap.tsx
+++ src/sections/Roadmap.tsx
@@ -31,7 +31,7 @@ const q4 = [
 
 export default function Roadmap() {
     return (
-        <div id="roadmap" className="section mt-20 sm:mt-0 py-10 px-20 w-full flex flex-col items-center justify-center" style={{backgroundColor: '#FAFAFA'}}>
+        <div id="roadmap" className="section mt-20 sm:mt-0 py-10 px-10 sm:px-20 w-full flex flex-col items-center justify-center" style={{backgroundColor: '#FAFAFA'}}>
             <div className="py-4 sm:py-10 text-2xl sm:text-5xl font-bold">ROADMAP</div>
             <div className="w-full grid grid-cols-1 sm:grid-cols-3 items-center justify-between">
                 <RoadmapItem title='Q2 2022' content={q2}></RoadmapItem>
